2696a57d352971a82227c2ccdf1dcadac71f01a9
JesusMoralesCa <jesus.morales.carr@gmail.com>
2023-12-12 14:00:28 +0100

    Finish JWT security

--- springboot-react/src/main/java/com/jmorales/springbootreact/Controller/AuthController.java
+++ springboot-react/src/main/java/com/jmorales/springbootreact/Controller/AuthController.java
@@ -2,16 +2,36 @@ package com.jmorales.springbootreact.Controller;
 
 import com.jmorales.springbootreact.Exception.UserAlreadyExistsException;
 import com.jmorales.springbootreact.Model.User;
+import com.jmorales.springbootreact.Request.LoginRequest;
+import com.jmorales.springbootreact.Response.JwtResponse;
+import com.jmorales.springbootreact.Security.User.ProjectUserDetails;
+import com.jmorales.springbootreact.Security.jwt.JwtUtils;
 import com.jmorales.springbootreact.Service.IUserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final IUserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
 
     @PostMapping("/register-user")
     public ResponseEntity<?> resgisterUser(User user){
@@ -21,6 +41,24 @@ public class AuthController {
         }catch (UserAlreadyExistsException e){
             return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest request){
+        Authentication authentication =
+                authenticationManager
+                        .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtTokenForUser(authentication);
+        ProjectUserDetails userDetails = (ProjectUserDetails) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority).toList();
+        return ResponseEntity.ok(new JwtResponse(
+                userDetails.getId(),
+                userDetails.getEmail(),
+                jwt,
+                roles));
     }
 
 }
---
+++ springboot-react/src/main/java/com/jmorales/springbootreact/Security/User/ProjectUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.jmorales.springbootreact.Security.User;
+
+import com.jmorales.springbootreact.Model.User;
+import com.jmorales.springbootreact.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return ProjectUserDetails.buildUserDetails(user);
+    }
+}
