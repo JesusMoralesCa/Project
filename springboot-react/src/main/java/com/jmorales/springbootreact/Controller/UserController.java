af7e852d1f9abeef03853d0ed0000e15c2de0f0e
JesusMoralesCa <jesus.morales.carr@gmail.com>
2023-12-12 14:35:16 +0100

    Fixes

--- springboot-react/src/main/java/com/jmorales/springbootreact/Controller/UserController.java
+++ springboot-react/src/main/java/com/jmorales/springbootreact/Controller/UserController.java
@@ -5,6 +5,7 @@ import com.jmorales.springbootreact.Service.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,11 +19,14 @@ public class UserController {
     private final IUserService userService;
 
     @GetMapping("/all")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<User>> getUsers(){
-        return new ResponseEntity<List<User>>(userService.getUsers(), HttpStatus.FOUND);
+
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.FOUND);
     }
 
     @GetMapping("/{email}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email){
         try{
             User theUser = userService.getUser(email);
@@ -35,6 +39,7 @@ public class UserController {
     }
 
     @DeleteMapping("/delete/{userId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or (hasRole('ROLE_USER') and #email == principal.username)")
     public ResponseEntity<String> deleteUser(@PathVariable("userId") String email){
         try {
             userService.deleteUser(email);
